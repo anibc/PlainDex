76bfc3eab1999b6acc32de8f2f5b4f23753a3633
another <oni74mail@gmail.com>
2019-11-17 16:30:57 +0600

    Added recent files list and textview

--- app/src/main/java/com/example/plaindex/MainActivity.kt
+++ app/src/main/java/com/example/plaindex/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.example.plaindex
 
 import android.app.Activity
+import android.opengl.Visibility
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import com.example.plaindex.databinding.ActivityMainBinding
 
 import kotlinx.android.synthetic.main.activity_main.*
@@ -17,6 +21,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
+        val model = ViewModelProviders.of(this)[MainViewModel::class.java]
         setContentView(binding.root)
         //setSupportActionBar(toolbar)
 
@@ -24,6 +29,12 @@ class MainActivity : AppCompatActivity() {
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }*/
+        var recfiles = model.getRecentFiles().value
+        binding.recentFilesTv.visibility = if(recfiles == null || recfiles.isEmpty()) View.INVISIBLE else View.VISIBLE
+        model.getRecentFiles().observe(this, Observer<List<String>>{ files ->
+            binding.recentFilesTv.text = files.size.toString()
+            binding.recentFilesTv.visibility = if (files.isEmpty()) View.INVISIBLE else View.VISIBLE
+        })
         binding.NewNoteButton.text = "New binding note"
     }
 
--- app/src/main/java/com/example/plaindex/MainViewModel.kt
+++ app/src/main/java/com/example/plaindex/MainViewModel.kt
@@ -1,6 +1,21 @@
 package com.example.plaindex
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MainViewModel: ViewModel() {
+    private val recentFiles: MutableLiveData<List<String>> by lazy {
+        MutableLiveData<List<String>>().also {
+            loadRecentFiles()
+        }
+    }
+
+    fun getRecentFiles(): LiveData<List<String>> {
+        return recentFiles
+    }
+
+    private fun loadRecentFiles() {
+        // Do an asynchronous operation to fetch users.
+    }
 }
